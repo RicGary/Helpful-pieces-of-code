c2754ee5dbf81abbbaa9b5dc6230b47536403d41
RicGary <ericnaiber@gmail.com>
2023-09-15 15:06:58 -0300

    Fixing pip install and adding option for Windows and Unix/MacOS

--- easy_setup.py
+++ easy_setup.py
@@ -3,29 +3,36 @@ Code to easy setup the environment, creates a dir for the data, updates pip and 
 """
 
 import os
-import sys
 import platform
+import sys
+
+from time import sleep
 
 def setup_dir():
     """Simple function to setup the enviroment."""
     files = os.listdir()
     python_version = platform.python_version()
+    op_system = platform.system()
+    act_path = os.getcwd()
 
-    if "data" not in files:
-        os.mkdir("data")
-        
-        try:
-            os.system("python -m venv venv")
-            os.system("python.exe -m pip install --upgrade pip")
-            os.system("pip install -r requirements.txt")
-        except:
-            os.system("python3 -m venv venv")
-            os.system("python.exe -m pip install --upgrade pip")
-            os.system("pip3 install -r requirements.txt")
+    if "data" in files:
+        print("Setup already made, shutting down...")
+        sleep(5)
+        sys.exit()
 
-        if python_version[0] != "3":
-            print("Please consider using Python 3!")
-            print("https://www.python.org/downloads/", "\n")
+    if op_system == "Windows":
+        os.system("python -m venv venv")
+        os.system("python -m pip install --upgrade pip setuptools wheel")
+        os.system(f"{act_path}/venv/Scripts/activate && pip install -r requirements.txt")
+
+    else:
+        os.system("python3 -m venv venv")
+        os.system("python3 -m pip install --upgrade pip setuptools wheel")
+        os.system(f"{act_path}/venv/Scripts/activate && pip install -r requirements.txt")
+
+    if python_version[0] != "3":
+        print("Please consider using Python 3!")
+        print("https://www.python.org/downloads/", "\n")
 
 
 if __name__ == "__main__":
