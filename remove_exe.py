806ae8dfb19cfc778db2212a75755f4be5ea1b03
RicGary <ericnaiber@gmail.com>
2024-02-21 23:07:28 -0300

    fixing typos

--- remove_exe.py
+++ remove_exe.py
@@ -7,7 +7,9 @@ How to run this code in cmd?
     1. pip install pyinstaller
     2. cd path\to\your\script
     3. pyinstaller --onefile your_script.py
-    4. 
+    4. Open dir folder
+    5. Copy your_script.exe 
+    6. Paste on you C:\<YOUR_PATH>\Python\Scripts
 """
 import os
 import sys
@@ -24,11 +26,16 @@ def main(argument) -> None:
 
     def red(string):
         return colorama.Fore.RED + string + colorama.Style.RESET_ALL
+    def green(string):
+        return colorama.Fore.GREEN + string + colorama.Style.RESET_ALL
 
     if argument == "-help":
         print(
-            "Removes " + red("all") + " .exe files on desired directory. Be careful. \n" +
-            "pyclean <PATH> \n"
+            "\nRemoves " + red("all") + " .exe files on desired directory. Please be careful. \n" +
+            "You need to be on the right directory to run the command. Saddly :( by the time you can't pass:\n" +
+            green("pyclean <PATH> \n") +
+            "as parameter. If you want to help implementing it go to:\n" +
+            "https://github.com/RicGary/Helpful-pieces-of-code"
         )
         sys.exit()
 
@@ -39,7 +46,7 @@ def main(argument) -> None:
     double_check = input(
         red("Do you want to delete all .exe files from: ") +
         f"\n {current_directory} \n"+
-        f"(y/n)? "
+        f"(y/n)? \n"
         )
 
     if double_check == "n":
